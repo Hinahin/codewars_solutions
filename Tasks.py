5a857f28c44c4c128ac8332288d54e5e8fb1e4fe
hinahin <hinahin.nightchild@gmail.com>
2020-03-24 23:37:33 +0500

    second task in pythonchellenge

--- Tasks.py
+++ Tasks.py
@@ -2,5 +2,24 @@ def check_import():
     return "Импортировано успешно"
 
 
+def second_task(code_text: str):
+    abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+           "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    result = ""
+
+    for word in code_text:
+        if word in abc:
+            if abc.index(word) > 24:
+                result += abc[1]
+            elif abc.index(word) > 23:
+                result += abc[0]
+            else:
+                result += abc[abc.index(word) + 2]
+        else:
+            result += word
+
+    return result
+
+
 if __name__ == '__main__':
     print("Привет мир!")
--- runtime.py
+++ runtime.py
@@ -1,3 +1,8 @@
 from Tasks import *
+str_q = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. " \
+      "bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. " \
+      "sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 
-print(check_import())
+
+print(second_task(str_q))
+print(second_task("map"))
